d1e53ed50272c505bff5cc7a6b788c099603ca5e
Aduda-Boaz <adudaboaz@gmail.com>
2023-04-06 22:17:47 +0300

    Fix the errors

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,53 +1,10 @@
-import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import supportServices from '../assets/img/support-services.png';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
-const Banner = () => {
-
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [, setIndex] = useState(1);
-  const toRotate = [ "Professionalism", "Dignity", "Integrity", "Patient", "Teamwork" ];
-  const period = 2000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-  
+const Banner = () => { 
   return (
     <section className="banner" id="home">
       <Container>
@@ -57,7 +14,7 @@ const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to Scride Consultancy</span>
-                <h1>{'We Value'} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Professionalism", "Dignity", "Integrity", "Patient", "Teamwork" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{'We Value'} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Professionalism", "Dignity", "Integrity", "Patient", "Teamwork" ]'><span className="wrap"></span></span></h1>
                   <p>We are committed to transforming and standardizing the organization’s operations for increased outcome attainment.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
